60bc77d185b22edb321a06ae5d1dd5280ad42e7b
Jrios05 <riosjose0305@gmail.com>
2026-05-24 22:20:08 -0500

    ajuste_ruteoPages

--- app.py
+++ app.py
@@ -6,18 +6,17 @@ st.set_page_config(page_title="Portal de Operaciones", layout="wide", initial_si
 # --- 1. DEFINIR LAS PÁGINAS ---
 
 # Módulo PAYMENT
-pay_creacion = st.Page("pages/pay_creacion.py", title="Creación de comercio", icon="🏢")
-pay_canales = st.Page("pages/pay_canales.py", title="Canales de pago", icon="💳")
-pay_actualizacion = st.Page("pages/pay_actualizacion.py", title="Actualización de cuenta", icon="🔄")
+pay_creacion = st.Page("pages/pyi_creacionComercio.py", title="Creación de comercio", icon="🏢")
+pay_canales = st.Page("pages/pyi_configuracion.py", title="Canales de pago", icon="💳")
 
 # Módulo PAYOUT
-payout_creacion = st.Page("pages/payout_creacion.py", title="Creación de comercio", icon="🏢")
-payout_config = st.Page("pages/payout_ruteo.py", title="Ruteo y titularidad", icon="⚙️")
+payout_creacion = st.Page("pages/pyo_creacionComercio.py", title="Creación de comercio", icon="🏢")
+payout_config = st.Page("pages/pyo_ruteo.py", title="Ruteo y titularidad", icon="⚙️")
 
 # --- 2. ARMAR LA NAVEGACIÓN CON LA JERARQUÍA DE LA IMAGEN ---
 pg = st.navigation({
     "Payment": [pay_creacion],
-    "Payment - Configuración": [pay_canales, pay_actualizacion],
+    "Payment - Configuración": [pay_canales],
     "Payout": [payout_creacion],
     "Payout - Configuración": [payout_config]
 })
